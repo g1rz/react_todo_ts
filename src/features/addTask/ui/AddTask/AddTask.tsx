90e5fcb7d83606abbca1905a40f65803c72c7f5f
Sergey Peshkov <zloi.g1rz@gmail.com>
2024-09-27 01:07:02 +0800

    add feature AddTask

---
+++ src/features/addTask/ui/AddTask/AddTask.tsx
@@ -0,0 +1,28 @@
+import { Button, TextField } from '~/shared';
+import styles from './AddTask.module.scss';
+import { FormEvent, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTask } from '~/entities/task';
+
+export function AddTask() {
+    const [textValue, setTextValue] = useState('');
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+
+        dispatch(addTask(textValue.trim()));
+        setTextValue('');
+    };
+
+    return (
+        <form className={styles.addTask} onSubmit={handleSubmit}>
+            <TextField
+                placeholder="enter task"
+                value={textValue}
+                onChange={(e) => setTextValue(e.target.value)}
+            />
+            <Button>add</Button>
+        </form>
+    );
+}
