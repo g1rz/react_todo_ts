627fff60b3e02a837466d60284e2c8e6f0407cfc
Sergey Peshkov <zloi.g1rz@gmail.com>
2024-09-27 16:42:27 +0800

    add ArrTask & TaskList on MainPage

--- src/pages/main/ui/MainPage.tsx
+++ src/pages/main/ui/MainPage.tsx
@@ -1,11 +1,15 @@
 import { Container, Text } from '~/shared';
 import styles from './MainPage.module.scss';
+import { AddTask, TaskList } from '~/features/task';
 
 export function MainPage() {
     return (
         <Container className={styles.page}>
             <Text as="h1">TODO list</Text>
-            <div className={styles.content}></div>
+            <div className={styles.content}>
+                <AddTask />
+                <TaskList />
+            </div>
         </Container>
     );
 }
